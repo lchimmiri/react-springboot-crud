0a4be09333c0cf19682a39cbdee468b11c49d7d7
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-10-02 20:45:16 -0500

    Added frontend react code

---
+++ react-frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <Route
+              path="/add-employee/:id"
+              component={CreateEmployeeComponent}
+            ></Route>
+            <Route
+              path="/view-employee/:id"
+              component={ViewEmployeeComponent}
+            ></Route>
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
